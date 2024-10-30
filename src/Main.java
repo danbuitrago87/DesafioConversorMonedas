426c7a0102b95d3426b4b24e602fc879f51cb95e
unknown <danbuitrago87@gmail.com>
2024-10-29 20:13:33 -0500

    DesafioConversorMonedas agregando entradas de usuario

--- src/Main.java
+++ src/Main.java
@@ -25,28 +25,52 @@ public class Main {
 
             switch (opcion){
                 case 1:
+                    System.out.println("Digite el valor a cambiar: ");
+                    double valor=entrada.nextDouble();
                     double rate1 = converter.convert("USD", "ARS");
+                    double resultado=valor*rate1;
                     System.out.println("Tasa de cambio de USD a ARS: " + rate1);
+                    System.out.println(valor + " Dolares Corresponde a: " + resultado + " Pesos Argentinos");
                     break;
                 case 2:
+                    System.out.println("Digite el valor a cambiar: ");
+                    double valor2=entrada.nextDouble();
                     double rate2 = converter.convert("ARS", "USD");
+                    double resultado2=valor2*rate2;
                     System.out.println("Tasa de cambio de ARS a USD: " + rate2);
+                    System.out.println(valor2 + " Pesos Argentinos Corresponde a: " + resultado2 + " Dolares");
                     break;
                 case 3:
+                    System.out.println("Digite el valor a cambiar: ");
+                    double valor3=entrada.nextDouble();
                     double rate3 = converter.convert("USD", "BRL");
+                    double resultado3=valor3*rate3;
                     System.out.println("Tasa de cambio de USD a BRL: " + rate3);
+                    System.out.println(valor3 + " Dolares Corresponde a: " + resultado3 + " Real Brasilero");
                     break;
                 case 4:
+                    System.out.println("Digite el valor a cambiar: ");
+                    double valor4=entrada.nextDouble();
                     double rate4 = converter.convert("BRL", "USD");
+                    double resultado4=valor4*rate4;
                     System.out.println("Tasa de cambio de BRL a USD: " + rate4);
+                    System.out.println(valor4 + " Reales Brasileros Corresponde a: " + resultado4 + " Dolares");
                     break;
                 case 5:
+                    System.out.println("Digite el valor a cambiar: ");
+                    double valor5=entrada.nextDouble();
                     double rate5 = converter.convert("USD", "COP");
+                    double resultado5=valor5*rate5;
                     System.out.println("Tasa de cambio de USD a COP: " + rate5);
+                    System.out.println(valor5 + " Dolares Corresponde a: " + resultado5 + " Pesos Colombianos");
                     break;
                 case 6:
+                    System.out.println("Digite el valor a cambiar: ");
+                    double valor6=entrada.nextDouble();
                     double rate6 = converter.convert("COP", "USD");
+                    double resultado6=valor6*rate6;
                     System.out.println("Tasa de cambio de COP a USD: " + rate6);
+                    System.out.println(valor6 + " Pesos Colombianos Corresponden a: " + resultado6 + " Dolares");
                     break;
                 case 7:
                     System.out.println("Saliendo");
